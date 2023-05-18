10a7400f92adb54892c684f3f9bf90a9daffccca
muhamedjaveed <muhamedjaveed15@gmail.com>
2023-05-19 01:58:30 +0530

    Javeed's new commit

--- src/test/java/com/pageexecution/ExecutionClass.java
+++ src/test/java/com/pageexecution/ExecutionClass.java
@@ -2,7 +2,7 @@ package com.pageexecution;
 
 public class ExecutionClass {
 public static void main(String[] args) {
-      System.out.println("Welcome to Java");
+      System.out.println("Hi, I'm Javeed");
 	System.out.println("Execution");
 }
 }
